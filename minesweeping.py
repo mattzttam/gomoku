ce094ea002f0fcb8d974ee074bacaacfd1e978f9
mattzttam <mattchow2011@outlook.com>
2024-08-17 11:14:00 +0800

    mine game

--- minesweeping.py
+++ minesweeping.py
@@ -1,63 +1,221 @@
-#mine sweeping
-import random
-def print_color(text, color):
-    colors = {
-        'black': '\033[30m',
-        'red': '\033[31m',
-        'green': '\033[32m',
-        'yellow': '\033[33m',
-        'blue': '\033[34m',
-        'magenta': '\033[35m',
-        'cyan': '\033[36m',
-        'white': '\033[37m'
-    }
-    reset = '\033[0m'
+from minesweeping_lib import *
+import pygame
+MOUSE_LEFT = 1
+MOUSE_RIGHT = 3
+block_empty = 'block_empty.png'
+block_one = 'block_one.png'
+block_two = 'block_two.png'
+block_three = 'block_three.png'
+block_four = 'block_four.png'
+block_five = 'block_five.png'
+block_six = 'block_six.png'
+block_nine = 'block_nine.png'
+block_light_nine = 'block_light_nine.png'
+block_flag = 'block_flag.png'
+block_unknowned = 'block_unknowned.png'
+mine_appear = 'mine_appear.png'
+mine_unexplosioned = 'mine_unexplosioned.png'
+mine_explosioned = 'mine_explosioned.png'
 
-    print(f"{colors[color]}\033[1;40m{text}{reset}", end='')
- 
-
-def init_board(c, l):
-    all_ls = []
-    for i in range(c):
-        line_ls = []
-        for j in range(l):
-            line_ls.append(0)
-        all_ls.append(line_ls)
-    return all_ls
-
-def pretty_print(board):
-    for line in board:
-        for b in line:
-            if b == 0:
-                print_color('# ', 'blue')
-            elif b == 9:
-                print_color('- ', 'white')
-            elif b == '*':
-                print_color('* ', 'red')
-            else:
-                text = str(b) + ' '
-                print_color(text, 'white')
-        print()
-
-def mine_rand(board):
-    l = random.randint(0, 24)#TODO
-    n = random.randint(0, 24)#TODO
-    if board[l][n] == '*':
-        mine_rand(board)
-    else:
-        board[l][n] = '*'
-
-def set_mine(num, board):
-    for i in range(num):
-        mine_rand(board)
-
-col = 25
-lin = 25
+#img_ls = [block_empty, block_one, block_two, block_three, block_four, block_five, block_six, \
+#          block_nine, block_light_nine, block_flag, block_unknowned, mine_appear, \
+#          mine_unexplosioned, mine_explosioned]
+COL = 9
+ROW = 9
 mine_num = 10
 
-n = int(int(col - 13) / 2)
-print('* ' * n + 'M I N E   S W E E P I N G ' + '* ' * n)
-print()
-board = init_board(col, lin)
-set_mine(mine_num, board)
-pretty_print(board)
+def check(b_col, b_row, ls):
+    if b_col + 1 < COL:
+        if not [b_row, b_col + 1] in ls:
+            ls.append([b_row, b_col + 1])
+            if board[b_row][b_col + 1] == 0:
+                check(b_col + 1, b_row, ls)
+
+    if b_col - 1 >= 0:
+        if not [b_row, b_col - 1] in ls:
+            ls.append([b_row, b_col - 1])
+            if board[b_row][b_col - 1] == 0:
+                check(b_col - 1, b_row, ls)
+
+    if b_row + 1 < ROW:
+        if not [b_row + 1, b_col] in ls:
+            ls.append([b_row + 1, b_col])
+            if board[b_row + 1][b_col] == 0:
+                check(b_col, b_row + 1, ls)
+
+    if b_row - 1 >= 0:
+       if not [b_row - 1, b_col] in ls:
+            ls.append([b_row - 1, b_col])
+            if board[b_row - 1][b_col] == 0:
+                check(b_col, b_row - 1, ls)
+
+
+    if b_col + 1 < COL and b_row + 1 < ROW:
+        if not [b_row + 1, b_col + 1] in ls:
+            ls.append([b_row + 1, b_col + 1])
+            if board[b_row + 1][b_col + 1] == 0:
+                check(b_col + 1, b_row + 1, ls)
+
+    if b_col - 1 >= 0 and b_row + 1 < ROW:
+        if not [b_row + 1, b_col - 1] in ls:
+            ls.append([b_row + 1, b_col - 1])
+            if board[b_row + 1][b_col - 1] == 0:
+                check(b_col - 1, b_row + 1, ls)
+
+    if b_col + 1 < COL and b_row - 1 >= 0:
+        if not [b_row - 1, b_col + 1] in ls:
+            ls.append([b_row - 1, b_col + 1])
+            if board[b_row - 1][b_col + 1] == 0:
+                check(b_col + 1, b_row - 1, ls)
+
+    if b_col - 1 >= 0 and b_row - 1 >= 0:
+       if not [b_row - 1, b_col - 1] in ls:
+            ls.append([b_row - 1, b_col - 1])
+            if board[b_row - 1][b_col - 1] == 0:
+                check(b_col - 1, b_row - 1, ls)
+
+    return ls
+
+def start_game(col, row):
+    board = init_board(col, row)
+    set_mine(mine_num, board)
+    set_number(board, col, row)
+    player_board = init_player_board(col, row)
+    return board, player_board
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.top, self.rect.left = location
+
+    def click(block, b_col, b_row):
+        if player_board[b_row][b_col] == '>':
+            return
+        to_open_ls = []
+
+        player_board[b_row][b_col] = board[b_row][b_col]
+        screen.blit(block.image, block.rect)
+        pygame.display.flip()
+
+        if board[b_row][b_col] == 0:
+            check(b_col, b_row, to_open_ls)
+
+        for pos in to_open_ls:
+            player_board[pos[0]][pos[1]] = board[pos[0]][pos[1]]
+
+
+    def light(x, y, block):
+        b_x = block.rect.x
+        b_y = block.rect.y
+        b_row = int(b_y / 35)
+        b_col = int(b_x / 35)
+
+        if (b_x < x < b_x + 35) and (b_y < y < b_y + 35):
+            if player_board[b_row][b_col] == 9:
+                player_board[b_row][b_col] = '^'
+        else:
+            if player_board[b_row][b_col] == '^':
+                player_board[b_row][b_col] = 9
+        #display_board()
+        screen.blit(block.image, block.rect)
+
+
+pygame.init()
+screen = pygame.display.set_mode((COL * 35, ROW * 35))
+pygame.display.set_caption("MINE SWEEPING")
+screen.fill([255, 255, 255])
+
+msg_font = pygame.font.Font(None, 32)
+
+board, player_board = start_game(COL, ROW)
+print(board)
+def display_board():
+    blocks = []
+    for i in range(ROW):
+        for j in range(COL):
+            location = [i * 35, j * 35]
+            if player_board[i][j] == 9:
+                block = Block(block_nine, location)
+            elif player_board[i][j] == 0:
+                block = Block(block_empty, location)
+            elif player_board[i][j] == '^':
+                block = Block(block_light_nine, location)
+            elif player_board[i][j] == 1:
+                block = Block(block_one, location)
+            elif player_board[i][j] == 2:
+                block = Block(block_two, location)
+            elif player_board[i][j] == 3:
+                block = Block(block_three, location)
+            elif player_board[i][j] == 4:
+                block = Block(block_four, location)
+            elif player_board[i][j] == 5:
+                block = Block(block_five, location)
+            elif player_board[i][j] == 6:
+                block = Block(block_six, location)
+            elif player_board[i][j] == '*':
+                block = Block(mine_appear, location)
+            elif player_board[i][j] == '>':
+                block = Block(block_flag, location)
+            elif player_board[i][j] == '?':
+                block = Block(block_unknowned, location)
+            blocks.append(block)
+    return blocks
+
+clock = pygame.time.Clock()
+while True:
+    blocks = display_board()
+    for block in blocks:
+        screen.blit(block.image, block.rect)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == MOUSE_LEFT:
+                x, y = pygame.mouse.get_pos()
+                blocks = display_board()
+                for block in blocks:
+                    b_x = block.rect.x
+                    b_y = block.rect.y
+                    b_row = int(b_y / 35)
+                    b_col = int(b_x / 35)
+
+                    if (b_x < x < b_x + 35) and (b_y < y < b_y + 35):
+                        Block.click(block, b_col, b_row)
+                continue
+            elif event.button == MOUSE_RIGHT:
+                x, y = pygame.mouse.get_pos()
+                blocks = display_board()
+                for block in blocks:
+                    b_x = block.rect.x
+                    b_y = block.rect.y
+                    b_row = int(b_y / 35)
+                    b_col = int(b_x / 35)
+
+                    original = player_board[b_row][b_col]
+                    if (b_x < x < b_x + 35) and (b_y < y < b_y + 35):
+                        if original != 9 and original != '^' and original != '>' and original != '?':
+                            continue;
+                        if original == 9 or original == '^':
+                            new_value = '>'
+                        elif original  == '>':
+                            new_value = '?'
+                        elif original == '?':
+                            new_value = '^'
+                        player_board[b_row][b_col] = new_value
+                        blocks = display_board()
+                        for block in blocks:
+                            screen.blit(block.image, block.rect)
+
+                continue
+        elif event.type == pygame.MOUSEMOTION:
+            x, y = pygame.mouse.get_pos()
+            blocks = display_board()
+            for block in blocks:
+                Block.light(x, y, block)
+
+    pygame.display.flip()
+    clock.tick(30)
--- minesweeping_lib.py
+++ minesweeping_lib.py
@@ -1,62 +1,62 @@
 #mine sweeping library
 import random
-def init_board(c, l):
-    all_ls = []
-    for i in range(c):
-        line_ls = []
-        for j in range(l):
-            line_ls.append(9)
-        all_ls.append(line_ls)
-    return all_ls
+COL = 9
+ROW = 9
+mine_num = 10
+#TODO
 
-def init_player_board(c, l):
-    all_ls = []
-    for i in range(c):
-        line_ls = []
-        for j in range(l):
-            line_ls.append(9)
-        all_ls.append(line_ls)
-    return all_ls
+def init_board(c, r):
+    all = []
+    for i in range(r):
+        one_row = []
+        for j in range(c):
+            one_row.append(9)
+        all.append(one_row)
+    return all
 
-def mine_rand(board, col, lin):
-    l = random.randint(0, col - 1)
-    n = random.randint(0, lin - 1)
-    if board[l][n] == '*':
-        mine_rand(board, col, lin)
+# TODO duplicated with init_board
+def init_player_board(c, r):
+    return init_board(c, r)
+
+def mine_rand(board, col, row):
+    c = random.randint(0, col - 1)
+    r = random.randint(0, row - 1)
+    if board[r][c] == '*':
+        mine_rand(board, col, row)
     else:
-        board[l][n] = '*'
+        board[r][c] = '*'
 
 def set_mine(num, board):
     for i in range(num):
-        mine_rand(board, col, lin)
+        mine_rand(board, COL, ROW)
 
-def set_number(board, col, lin):
+def set_number(board, max_col, max_row):
     for i in range(len(board)):
         for j in range(len(board[i])):
             if board[i][j] == '*':
                 continue
             count = 0
-            if not((i + 1) > lin - 1):
+            if ((i + 1) <= max_row - 1):
                 if board[i + 1][j] == '*':
                     count += 1
-            if not((i - 1) < 0):
+            if ((i - 1) >= 0):
                 if board[i - 1][j] == '*':
                     count += 1
-            if not((j + 1) > col - 1):
+            if ((j + 1) <= max_col - 1):
                 if board[i][j + 1] == '*':
                     count += 1
-            if not((j - 1) < 0):
+            if ((j - 1) >= 0):
                 if board[i][j - 1] == '*':
                     count += 1
-            if not((i + 1) > lin - 1):
-                if not((j + 1) > col - 1):
+            if not((i + 1) > max_row - 1):
+                if not((j + 1) > max_col - 1):
                     if board[i + 1][j + 1] == '*':
                         count += 1
             if not((i - 1) < 0):
-                if not((j + 1) > col - 1):
+                if not((j + 1) > max_col - 1):
                     if board[i - 1][j + 1] == '*':
                         count += 1
-            if not((i + 1) > lin - 1):
+            if not((i + 1) > max_row - 1):
                 if not((j - 1) < 0):
                     if board[i + 1][j - 1] == '*':
                         count += 1
